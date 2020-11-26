b828cf90a409707e8cd967a063c23a5f85d844ff
ashaheed00 <ashaheed00@gmail.com>
2020-11-26 11:35:46 +0530

    Adding from UC1 to UC6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./aseets/img/BL_logo_square_png.png";
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  url = "https://www.bridgelabz.com/";
+  constructor() {
+    super();
+    this.state = {
+      title: "Hi ",
+      userName: "Foo",
+      nameError: "Eg. John",
+    };
+  }
+
+  onClick = ($event) => {
+    console.log("blz", $event);
+    window.open(this.url, "_blank");
+  };
+
+  onNameChange = (event) => {
+    console.log("username: ", event.target.value);
+    const nameRegex = RegExp("^[A-Z][a-z]{2,}");
+    this.setState({ userName: event.target.value });
+    if (nameRegex.test(event.target.value)) this.setState({ nameError: "" });
+    else this.setState({ nameError: "Wrong name format" });
+  };
+
+  render() {
+    return (
+      <>
+        <div>
+          <h1>
+            {this.state.title}
+            {this.state.userName}
+          </h1>
+          <img src={logo} onClick={this.onClick} alt="The BridgeLabz"></img>
+        </div>
+        <div>
+          Username: <input onChange={this.onNameChange}></input>
+          <span class="error-output" onChange={this.state.onNameChange}>
+            {this.state.nameError}
+          </span>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
